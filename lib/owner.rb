4bc0848fe859930c33333b154a65a950d3a8f9fb
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 18:16:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -76,4 +76,8 @@ class Owner
     @pets = {:fishes => [], :dogs => [], :cats => []}
   end 
   
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end 
+  
 end
